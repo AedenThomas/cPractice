63b4ec2f4cd1ac321c8c6748a7351346b70d06a5
Aeden Thomas <aedengeo@gmail.com>
2024-02-27 12:53:34 +0400

    Remove DSC directory from Git tracking

--- DSC/HistoryOfMysore.c
+++
@@ -1,6 +0,0 @@
-// Essay about the history of Mysore
-
-// History of Mysore
-// Mysore was the capital of the Kingdom of Mysore in the 11th century.
-// The city was built on the site of the ancient city of Mysore, which was built by the Mysore kings. 
-// 
